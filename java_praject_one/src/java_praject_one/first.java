940ffc2ffe21cf91253c9fd61238d891a2c8b601
nareshvaddi <nareshvaddi88@gmail.com>
2021-10-28 19:19:50 +0530

    project configured

---
+++ java_praject_one/src/java_praject_one/first.java
@@ -0,0 +1,11 @@
+package java_praject_one;
+
+public class first {
+		public static void main(String s[])
+		{
+			System.out.println("Main method");
+
+			System.out.println("Main method");
+		}
+
+}
